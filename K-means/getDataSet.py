8b66caa85c17324302d6b1b584044261e4f2894c
AoWenjie <wenjieao97@gmail.com>
2019-01-17 15:48:29 +0800

    添加生成数据集的源代码

---
+++ K-means/getDataSet.py
@@ -0,0 +1,10 @@
+#专用于生成一些数据集
+#作者：敖文杰
+#日期：2019/01/09
+import random
+
+f = open('k_means_dataSet.txt','w+')
+
+for i in range(100):
+    f.write(str(round(random.uniform(0,20),2)) + '\t' +str(round(random.uniform(0,20))) + '\n')
+    f.write(str(round(random.uniform(20,40),2)) + '\t' +str(round(random.uniform(20,40))) + '\n')
